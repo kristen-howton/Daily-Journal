9277940620ea9266cfd0cdff126a69629e0fcfc2
kristen-howton <kristen.howton@gmail.com>
2020-03-18 22:11:46 -0500

    added post and factory

--- scripts/form/JournalForm.js
+++ scripts/form/JournalForm.js
@@ -1,26 +1,57 @@
+import { saveJournalEntry } from "../JournalDataProvider.js"
+
 const contentTarget = document.querySelector(".journalFormContainer")
+const eventHub = document.querySelector(".container")
+const dateTarget = document.querySelector("#journalDate")
+const conceptTarget = document.querySelector("#conceptsCovered")
+const moodTarget = document.querySelector("#moodForTheDay")
+const buttonTarget = document.querySelector("#saveJournalEntryButton")
+
+
+
+const createEntry = (date, concept, entry, mood) => {
+    return {
+        date: date,
+        concept: concept,
+        entry: entry,
+        mood: mood
+    }
+}
+
+eventHub.addEventListener("click", event => {
+    console.log("clicked")
+    const newEntryObject = createEntry(dateTarget.value, conceptTarget.value, moodTarget.value, buttonTarget.value)
+    saveJournalEntry(newEntryObject)
+        .then(
+            // Dispatch new custom event that state has changed
+        )
+    return false
+})
+
+
+
 
 const render = () => {
     contentTarget.innerHTML = `
-        <form action="">
+        <form>
         <fieldset>
             <label for="journalDate">Date of entry</label>
-            <input type="date" name="journalData" id="journalData">
+            <input type="date" name="journalDate" id="journalDate">
         </fieldset>
 
         <fieldset>
             <label for="conceptsCovered">Concepts covered</label>
-            <input type="text">
+            <input type="text" id="conceptsCovered"> 
         </fieldset>
 
         <fieldset>
             <label for="journalEntry">Journal Entry</label>
-            <textarea name="Journal Entry" id="" cols="30" rows="10"></textarea>
+            <textarea name="Journal Entry" id="journalEntry" cols="30" rows="10"></textarea>
         </fieldset>
 
         <fieldset>
-            <label for="moodForTheDay">Mood for the day</label>
-            <select name="Mood for the day" id="">
+            <label for="moodForTheDay" id="moodForTheDay">Mood for the day</label>
+            <select name="Mood for the day">
                 <option value="Optimistic">Optimistic</option>
                 <option value="Motivated" class="value">Motivated</option>
                 <option value="Productive" class="value">Productive</option>
@@ -31,10 +62,7 @@ const render = () => {
                 <option value="Incapable" class="value">Incapable</option>
             </select>
         </fieldset>
-        <button>
-            <option value="recordJournalEntry">Record Journal Entry</option>
-        </button>
-        </fieldset>
+        <button id="saveJournalEntryButton">Record Journal Entry</button>
     </form>
     `
 }
